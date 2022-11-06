fa2196c7a88e238821e985d4fda683a8bc81aa04
B.Bhuvaneswari <110654513+Bandlapalli-Bhuvana@users.noreply.github.com>
2022-11-06 17:06:10 +0530

    Add files via upload

---
+++ GreatestAmongThreeNumbers.java
@@ -0,0 +1,15 @@
+
+public class GreatestAmongThreeNumbers {
+
+	public static void main(String[] args) {
+		int a=90,b=90,c=50;
+		if(a>=b && a>=c)
+			System.out.println(a +" is largest number");
+		else if(b>=a && b>=c)
+			System.out.println(b +" is largest number");
+		else
+			System.out.println(c +" is largest number");
+
+	}
+
+}
